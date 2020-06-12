07cd5c6267c2e6398584dc5da1748a174f0dd3c8
Muhammad Saleem Raza <salimraza3465@gmail.com>
2020-06-12 15:56:24 +0500

    Bootcamp2020 - Assg # 4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import {Container, Button} from '@material-ui/core'
+import Counter from './component/Counter'
 import './App.css';
 
 function App() {
+
+  let [count, updateCount] = useState(0) // passing initial value in useState
+  let [morning, updateMorning] = useState(false) // passing initial value in useState
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <Container className={` box  ${ morning? '' : 'night' } `}>
+      <br/>
+      <h2>Good {morning ? 'Morning' : 'Night'}</h2>
+
+      <Counter counter={count} /> {/* New Component */}
+
+      <Button variant="contained" color="primary" onClick={() => updateCount(count + 1)}>
+        Counter
+      </Button>
+    <br/> <br/>
+      <Button variant="contained" color="secondary" onClick = { () => updateMorning(!morning) }>
+        { !morning? 'Morning' : 'Night' } Theme
+      </Button>
+    <br/><br/>
+    </Container>
   );
 }
 
---
+++ src/component/Counter.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Counter(props){
+    return (
+    <h3>Counter: { props.counter }</h3>
+    )
+}
+
+// export default Counter
